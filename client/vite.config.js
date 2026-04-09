4a79c0bec11c0cff15afe1579efff3f1ccb7ea90
Sasi <johnreysasi55@gmail.com>
2026-04-10 07:41:17 +0800

    feat: configure Vite dev server with custom port and allowed ngrok host

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // or whatever plugin you use
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server: {
+  //   port: 8080,
+  //   allowedHosts: [
+  //     'charlyn-thankworthy-unshrewdly.ngrok-free.dev'
+  //   ]
+  // }
 })
